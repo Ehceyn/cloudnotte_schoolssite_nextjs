9748a3c41c010d96ab9e7b6279492dad851b8536
Chimeruzee <68004888+Ehceyn@users.noreply.github.com>
2022-03-25 22:31:26 +0100

    few changes

--- .history/lib/apolloClient_20220325221040.js
+++ .history/lib/apolloClient_20220325221040.js
@@ -12,7 +12,7 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === "undefined",
     link: new HttpLink({
-      uri: "https://api.cloudnotte.com/graphql/query", // Server URL (must be absolute)
+      uri: "https://api.cloudnotte.com/", // Server URL (must be absolute)
       credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache(),
